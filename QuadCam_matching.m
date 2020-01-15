88649709c2fae842f8b0114bb3d29d1fd262b970
Ellin Zhao <ellinzhao@berkeley.edu>
2020-01-15 15:36:26 -0800

    updated measurement orientation code

--- QuadCam_matching.m
+++ QuadCam_matching.m
@@ -4,7 +4,7 @@ PSF = imread('RPi_PSF.png');
 i = 1;
 for c = ['A' 'B' 'C' 'D']
     fprintf('Matching image %s', c);
-    cam = imread(sprintf('2019_11_18-orientation-4000/%s.png', c));
+    cam = imread(sprintf('2020_01_14/orientation/%s.png', c));
 
     [PSF_points, cam_points] = cpselect(cam, PSF, 'Wait', true);
     tform = fitgeotrans(PSF_points, cam_points, 'similarity');
@@ -17,4 +17,4 @@ for c = ['A' 'B' 'C' 'D']
     i = i + 1;
 end    
 
-save('cam_tforms.mat', 'cam_tforms')
+save('cam_tforms_new.mat', 'cam_tforms')
--- QuadCam_measurement.m
+++ QuadCam_measurement.m
@@ -1,5 +1,5 @@
-load 'cam_tforms.mat'
-generate_mask = false;
+load '2020_01_14/cam_tforms.mat'
+mode = 'measurement'; % 'orientation', 'mask', 'measurement'
 
 PSF = imread('RPi_PSF.png');
 [height, width, channels] = size(PSF);
@@ -7,15 +7,15 @@ panorama = zeros([height width 3], 'like', PSF);
 blender = vision.AlphaBlender('Operation', 'Binary mask', 'MaskSource', 'Input port');
 panoramaView = imref2d([width height]);
 mask = true(size(PSF,1), size(PSF,2));
-if generate_mask
+if ~strcmp(mode, 'orientation')
     PSF = uint8(zeros(size(PSF)));
 end
 panorama = step(blender, panorama, PSF, mask);
 
 i = 1;
 for c = ['A', 'B', 'C', 'D']
-    cam = imread(sprintf('2019_11_18-orientation-4000/%s.png', c));
-    if generate_mask
+    cam = imread(sprintf('2020_01_14/star_shift_more/%s.png', c));
+    if strcmp(mode, 'mask')
         cam = 255*uint8(ones(size(cam)));
     end    
     mask = imwarp(true(size(cam,1),size(cam,2)), cam_tforms(i), 'OutputView', panoramaView);
@@ -25,8 +25,5 @@ for c = ['A', 'B', 'C', 'D']
 end
 
 imshow(panorama);
-filename = 'QuadCam-orientations.png';
-if generate_mask
-    filename = 'QuadCam-mask.png';
-end
+filename = sprintf('%s.png', mode);
 imwrite(panorama, filename);
